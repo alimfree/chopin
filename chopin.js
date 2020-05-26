099ae01afc42047e6476de702653173bc2767306
Mohammed Ali <mali@teladoc.com>
2020-05-26 11:12:13 -0500

    checking duplicate files

--- chopin.js
+++ chopin.js
@@ -1,31 +1,114 @@
 const yargs = require('yargs');
 const path = require('path');
 const fs = require('fs');
+const util = require('util');
+const _ = require('lodash');
+
+/**
+ * Walks acceptance suite path and looks for all
+ * files.
+ * 
+ * @param {*} dir 
+ * @param {*} done 
+ */
+var findFiles = function(dir, done) {
+  var results = [];
+   fs.readdir(dir, function(err, list) {
+    if (err) return done(err);
+    var pending = list.length;
+    if (!pending) return done(null, results);
+    list.forEach(function(file) {
+      file = path.resolve(dir, file);
+        fs.stat(file, function(err, stat) {
+          if (stat && stat.isDirectory()) {
+          findFiles(file, function(err, res) {
+            results = results.concat(res);
+            if (!--pending) done(null, results);
+          });
+        } else {
+        results.push(file);
+        if (!--pending) done(null, results);
+        }
+      });
+    });
+  });
+};
+
+/*
+ * Sorts hash in descending order, most duplicated
+ * steps in begining of hash.
+ */
+function sortDescending(unordered){
+  let ordered = {};
+  Object.keys(unordered).sort().forEach(function(key) {
+    ordered[key] = unordered[key];
+  });
+  return ordered
+}
+
+/*
+ * Custom merge method to add all duplicates across
+ * files.
+ */
+function mergeDuplicates(objValue, srcValue){
+  if (_.isNumber(objValue)) {
+    return objValue + srcValue;
+  }
+}
+  
+/*
+* Counts each occurrence of a step inside a feature
+*/
+async function findDuplicateSteps(file){
+  let lineReader = require('readline').createInterface({
+    input: require('fs').createReadStream(file)
+  });
+  let duplicateSteps = {};
+
+  for await (let line of lineReader) {
+    line = line.trim()
+    if(skip(line)) continue
+    duplicateSteps[line] = duplicateSteps[line] || 0;
+    duplicateSteps[line]++;
+  }
+  return duplicateSteps;
+}
+  
+/*
+ * Skip commented out steps and documentation 
+ */
+function skip(line){
+  return (line[0] === "#" || line[0] === "@" || line === "") 
+}
 
 const argv = yargs
-    .command('chopin', 'Scans regression suite and outputs duplicate lines in descending order', {
-        dir: {
-            description: 'the acceptance suite directory path',
-            alias: 'd',
+  .command('chopin', 'Scans regression suite and outputs duplicate lines in descending order', {
+      dir: {
+          description: 'the acceptance suite directory path',
+          alias: 'd',
             type: 'string',
-        }
-    })
-    .help()
-    .alias('help', 'h')
-    .argv;
+      }
+  })
+  .help()
+  .alias('help', 'h')
+  .argv;
+
+
+let promiseFindFiles = util.promisify(findFiles)
 
 if (argv.dir) {
-  const acceptanceSuitePath = path.join(__dirname, argv.dir, 'features');
+  console.log('Searching for duplicate steps...')
+  const acceptanceSuitePath = path.join(argv.dir, 'features');
   //passsing directoryPath and callback function
-  fs.readdir(acceptanceSuitePath, function (err, files) {
-      //handling error
-      if (err) {
-          return console.log('Unable to scan acceptance suite directory: ' + err);
-      }
-      //listing all files using forEach
-      files.forEach(function (file) {
-          // Do whatever you want to do with the file
-          console.log(file);
-      });
+  promiseFindFiles(acceptanceSuitePath).then(function(files) {
+    return Promise.all(files.map(function(file){
+      return findDuplicateSteps(file)
+    }));
+  }).then(function(results){
+    let resultObject = results.reduce(function(resultObject, duplicates){
+      return _.mergeWith(resultObject, duplicates, mergeDuplicates);
+    }, {});
+    console.log(sortDescending(resultObject))
   });
 }
+
